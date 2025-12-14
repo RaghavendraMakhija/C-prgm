4070e54dccfc7cd39b13998ea560d51bf1afaede
Raghavendra Makhija <rmakhija.help@gmail.com>
2025-12-14 13:38:54 +0530

    Updated problem4.c

--- problem4.c
+++ problem4.c
@@ -1,22 +1,13 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char pass[10], correct[] = "mayone";
-    int attempts = 3;
-
-    while(attempts--) {
-        printf("Enter password: ");
-        scanf("%s", pass);
-
-        if(strcmp(pass, correct) == 0) {
-            printf("Login successful.\n");
-            return 0;
-        }
-        else
-            printf("Wrong password. Attempts left: %d\n", attempts);
-    }
-
-    printf("Account Locked.\n");
-    return 0;
+int main(){
+char pass[20], correct[]="admin123";
+for(int i=1;i<=3;i++){
+printf("Enter password: ");
+scanf("%s", pass);
+if(strcmp(pass,correct)==0){
+printf("Login Successful"); return 0;
+}
+printf("Wrong password! Attempts left: %d\n",3-i);
+}
+printf("Account locked.");
+return 0;
 }
