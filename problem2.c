6c091c6e4a34915ebced760a0de3c709b913cac5
Raghavendra Makhija <rmakhija.help@gmail.com>
2025-12-14 13:37:37 +0530

    Updated problem2.c

--- problem2.c
+++ problem2.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int sec, h, m, s;
+int main(){
+    int sec,a,b,c=1;
+    
     printf("Enter seconds: ");
-    scanf("%d", &sec);
-
-    h = sec / 3600;
-    sec %= 3600;
-    m = sec / 60;
-    s = sec % 60;
-
-    printf("Hours: %d, Minutes: %d, Seconds: %d\n", h, m, s);
+    if(scanf("%d", &sec)!=1 || sec<0 ){
+        printf("Invalid input! Enter positive integer.");
+        return 0; 
+    if (scanf("%d", &a,&b,&c)!=1)
+    {
+      printf("Invalid input! Enter positive integer.");
+      return 0;
+    }
+    
+    }
+ 
+    printf("%d:%d:%d", sec/3600, (sec%3600)/60, sec%60);
+    
     return 0;
 }
