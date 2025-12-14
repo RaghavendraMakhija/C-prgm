fcd1b3e0ea01eb26256e7c8b9703a22c2e9d8068
Raghavendra Makhija <rmakhija.help@gmail.com>
2025-12-14 13:41:19 +0530

    Updated problem8.c

--- problem8.c
+++ problem8.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    printf("Enter size: ");
-    scanf("%d", &n);
-
-    int a[n];
-    printf("Enter elements: ");
-    for(int i = 0; i < n; i++) scanf("%d", &a[i]);
-
-    for(int i = 0; i < n - 1; i += 2) {
-        int *p = &a[i];
-        int *q = &a[i+1];
-        int temp = *p;
-        *p = *q;
-        *q = temp;
+int main(){
+    int n, a[50];
+    printf("Enter size (1-50): ");
+    if(scanf("%d",&n)!=1 || n<=0 || n>50)
+    {
+        printf("Invalid size! Enter an integer between 1 and 50.");
+        return 0;
     }
 
-    printf("After swapping: ");
-    for(int i = 0; i < n; i++) printf("%d ", a[i]);
+    printf("Enter %d integers:", n);
+    for(int i=0;i<n;i++)
+    {
+        if(scanf("%d",&a[i])!=1){
+            printf("Wrong input! Only integers allowed.");
+            return 0; }
+    }
+
+    for(int i=0;i<n-1;i+=2)
+    {
+        int t=a[i];
+        a[i]=a[i+1];
+        a[i+1]=t;
+    }
+
+    printf("Result: ");
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+
     return 0;
 }
