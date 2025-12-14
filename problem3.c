2c392c29d0f015b928e8c33bae10bfeb42b84fa4
Raghavendra Makhija <rmakhija.help@gmail.com>
2025-12-14 13:38:31 +0530

    Updated problem3.c

--- problem3.c
+++ problem3.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int units;
-    float bill = 0;
-
-    printf("Enter total units: ");
-    scanf("%d", &units);
-
-    if(units <= 100)
-        bill = units * 5;
-    else if(units <= 300)
-        bill = 100 * 5 + (units - 100) * 8;
-    else
-        bill = 100 * 5 + 200 * 8 + (units - 300) * 10;
-
-    printf("Total Bill: %.2f\n", bill);
+int main(){
+    int units; float bill=0;
+printf("Enter units consumed: ");
+if(scanf("%d", &units)!=1 || units<0){
+printf("Invalid input!"); return 0;
+}
+if(units<=100) bill=units*5;
+else if(units<=300) bill=100*5+(units-100)*8;
+else bill=100*5+200*8+(units-300)*10;
+printf("Total Bill = rupees%.2f", bill);
     return 0;
 }
