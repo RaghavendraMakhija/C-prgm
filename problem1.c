0a1b316d2ad9fb101dca4c30f7c30bffd3457d10
Raghavendra Makhija <rmakhija.help@gmail.com>
2025-12-15 11:59:38 +0530

    Updated1 problem1.c

--- problem1.c
+++ problem1.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int a,b,c,sum,prime=1;
+    int a, b, c, sum;
+    int prime = 1;
+
     printf("Enter three integers: ");
-    if(scanf("%d %d %d", &a,&b,&c)!=3){
+    if (scanf("%d %d %d", &a, &b, &c) != 3) {
         printf("Wrong input! Please enter integers only.");
         return 0;
     }
-    sum = a+b+c;
+
+    sum = a + b + c;
     printf("Sum = %d\n", sum);
-    printf("Sum is %s\n", (sum%2==0)?"Even":"Odd");
-    if(sum<=1) prime=0;
-    for(int i=2;i*i<=sum;i++)
-        if(sum%i==0) prime=0;
-    printf("Sum is %s", prime?"Prime":"Composite");
+    printf("Sum is %s\n", (sum % 2 == 0) ? "Even" : "Odd");
+
+    if (sum <= 1) {
+        printf("Sum is neither Prime nor Composite");
+        return 0;
+    }
+
+    for (int i = 2; i * i <= sum; i++) {
+        if (sum % i == 0) {
+            prime = 0;
+            break;
+        }
+    }
+
+    printf("Sum is %s", prime ? "Prime" : "Composite");
     return 0;
 }
