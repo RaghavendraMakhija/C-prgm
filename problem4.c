a1970c4e94d28077f22c5ed5cead29a2d4fbde0b
Raghavendra Makhija <rmakhija.help@gmail.com>
2025-12-19 19:51:58 +0530

    Update problem4.c

--- problem4.c
+++ problem4.c
@@ -1,13 +1,28 @@
-int main(){
-char pass[20], correct[]="admin123";
-for(int i=1;i<=3;i++){
-printf("Enter password: ");
-scanf("%s", pass);
-if(strcmp(pass,correct)==0){
-printf("Login Successful"); return 0;
+/*Create a program to simulate a simple login system with 3 attempts to enter the correct password. */
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char correct[20], pass[20];
+    printf("Set your password: ");
+    scanf("%19s", correct);
+    for (int i = 1; i <= 3; i++)
+    {
+        printf("Enter password: ");
+        scanf("%19s", pass);
+        if (strcmp(pass, correct) == 0)
+        {
+            printf("Login Successful\n");
+            return 0;
+        }
+        printf("Wrong password! Attempts left: %d\n", 3 - i);
+    }
+    printf("Account locked.\n");
+    return 0;
 }
-printf("Wrong password! Attempts left: %d\n",3-i);
-}
-printf("Account locked.");
-return 0;
-}
+/*•	Code Logic
+-	By use of scanf to set a password of up to 19 characters.
+-	Allows the user up to 3 attempts to enter the correct password.
+-	Compares each input with the stored password using string comparison.
+-	Prints "Login Successful" and exits if the password matches.
+-	Locks the account after 3 incorrect attempts.*/
