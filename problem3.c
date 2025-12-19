995cf20f9a53dc02e681f87a8246322e9e3c95a2
Raghavendra Makhija <rmakhija.help@gmail.com>
2025-12-19 19:51:09 +0530

    Update problem3.c

--- problem3.c
+++ problem3.c
@@ -1,14 +1,15 @@
+/*Create a program to calculate electricity bills based on usage slabs (e.g.if total units are 350 then 0–100
+units: ₹5/unit, 101–300 units: ₹8/unit, ₹10/unit for units above 300, then total bill is of 350 units is
+2900). */
 #include <stdio.h>
-
-int main() {
+int main(){
     float units, bill = 0;
-
     printf("Enter units consumed: ");
-    if (scanf("%f", &units) != 1 || units < 0) {
+    if (scanf("%f", &units) != 1 || units < 0)
+    {
         printf("Invalid input!\n");
         return 1;
     }
-
     if (units <= 100)
         bill = units * 5;
     else if (units <= 300)
@@ -19,3 +20,7 @@ int main() {
     printf("Total Bill = rupees %.2f only.\n", bill);
     return 0;
 }
+/*•	Code Logic
+-	Uses scanf to accept the number of electricity units consumed, rejecting invalid input.
+-	Computes the bill incrementally based on the slabs.
+-	Displays the total bill in rupees.*/
